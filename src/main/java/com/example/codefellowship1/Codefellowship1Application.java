a48b1b1c85a4a60c29ec471b7240ce106adef3b7
abdallahhamoury <abdallah.hamoury@gmail.com>
2021-10-31 19:08:12 +0200

    update

---
+++ src/main/java/com/example/codefellowship1/Codefellowship1Application.java
@@ -0,0 +1,13 @@
+package com.example.codefellowship1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Codefellowship1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Codefellowship1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/codefellowship1/Codefellowship1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codefellowship1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Codefellowship1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
